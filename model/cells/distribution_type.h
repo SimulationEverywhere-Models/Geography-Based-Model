87631bff61892a33e24af9cb31f5c03dd9b01802
Román Cárdenas <rcardenas.rod@gmail.com>
2020-08-31 17:08:29 +0200

    Comments

--- model/cells/distribution_type.h
+++ model/cells/distribution_type.h
@@ -14,7 +14,15 @@ enum class distribution_type {
 
 /// Keeps track of what distribution to use when a value is required
 struct distribution {
+private:
 
+    std::pair<unsigned int, unsigned int> valid_phase_rate_indexes;
+    std::pair<double, double> max_range_values;
+    distribution_type distribution_id;
+    std::default_random_engine random_engine;
+
+    std::normal_distribution<double> normal_distribution;
+public:
     explicit distribution(std::pair<unsigned int, unsigned int> valid_phase_indexes, std::pair<double, double> max_range_values)
                             :
                                 valid_phase_rate_indexes{valid_phase_indexes},
@@ -38,21 +46,19 @@ struct distribution {
     }
 
     double get_value() {
-
          double value = 0.0;
-
          switch(distribution_id) {
-
-             case distribution_type ::Fixed:
-                 break;
-
              case distribution_type ::Normal:
                 value = normal_distribution(random_engine);
                 break;
+             default:  // Fixed
+                 break;
          }
 
          // Ensure that the given value is within the acceptable value; if it is not, then return the value that is closest
          // to an acceptable value.
+         // TODO aren't the max values 0 and 1 always?
+         // TODO Why don't you do: return std::min(max_range_values.second, std::max(max_range_values.first, value));
          if(value < max_range_values.first || value > max_range_values.second) {
             if(std::abs(value - max_range_values.first) < std::abs(value - max_range_values.second)) {
                 return max_range_values.first;
@@ -61,18 +67,8 @@ struct distribution {
                 return max_range_values.second;
             }
         }
-
         return value;
     }
-
-    private:
-
-        std::pair<unsigned int, unsigned int> valid_phase_rate_indexes;
-        std::pair<double, double> max_range_values;
-        distribution_type distribution_id;
-        std::default_random_engine random_engine;
-
-        std::normal_distribution<double> normal_distribution;
 };
 
 #endif //PANDEMIC_GEOGRAPHICAL_MODEL_DISTRIBUTION_TYPE_H
--- model/cells/phase_rates_object.h
+++ model/cells/phase_rates_object.h
@@ -12,6 +12,10 @@
 /// Encapsulates all of the phase rates for different age groups
 class phase_rates_object
 {
+    private:
+        // Mutable as the get_value_at() function must be const, but certain distributions,
+        // such as normal, internally mutate their state when getting a value from them.
+        mutable std::vector<std::vector<distribution>> distributions;
     public:
 
         void set_distribution_indexes(std::vector<std::vector<distribution>> &distributions) {
@@ -24,16 +28,8 @@ class phase_rates_object
                     return i.get_value();
                 }
             }
-
             throw std::runtime_error{"No valid phase index of: " + std::to_string(phase_index)};
         }
-
-    private:
-
-        // Mutable as the get_value_at() function must be const, but certain distributions, such as normal, internally mutate
-        // their state when getting a value from them.
-        mutable std::vector<std::vector<distribution>> distributions;
 };
-
 
 #endif //PANDEMIC_GEOGRAPHICAL_MODEL_PHASE_RATES_OBJECT_H
